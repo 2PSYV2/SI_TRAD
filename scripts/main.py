579a58a3435631d68c3d133dc338c874ca99aac8
Yevhenii Edelshteyn <yevheniiedelshteyn17@gmail.com>
2025-11-06 14:11:28 +0100

    initial commit

---
+++ scripts/main.py
@@ -0,0 +1,10 @@
+import opentracing
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
